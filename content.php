153c1a814c4f2a9d893e659d4b7563ede07e1e8c
Jelle Bol <jellebol2005@outlook.com>
2023-03-29 09:31:24 +0200

    Versie 3

---
+++ content.php
@@ -0,0 +1,6 @@
+<?php
+$naam = "Jelle Bol";
+?>
+
+<img src="ondernemer.png" alt="ondernemer">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae leo eget est elementum faucibus a sed velit. Nullam ac luctus sapien. Nunc maximus urna id sem dignissim vestibulum. Quisque dignissim tellus ut metus convallis sagittis.</p>
--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<?php
-require('variables.php');
-foreach($fruits as $fruit){
-  echo $fruit . "<br>";
-}
-?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Mijn Website</title>
+</head>
+<body>
+    <header>
+        <h1>Mijn Website</h1>
+    </header>
+    <main>
+        <?php include 'content.php'; ?>
+    </main>
+    <footer>
+        <p>&copy; Jelle Bol <?php echo date('Y'); ?></p>
+    </footer>
+</body>
+</html>
